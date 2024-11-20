c79e101f4eb725a5818b612fe010f6f6da9dabdc
nakulshah <nakulshah@gmail.com>
2024-11-19 23:34:24 -0600

    let's do some kotlin

--- src/main/java/com/nakulshah/Main.kt
+++ src/main/java/com/nakulshah/Main.kt
@@ -1,17 +1,163 @@
-package com.nakulshah;
+package com.nakulshah
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.print("Hello and welcome!");
+fun main(){
+    println("hello main!")
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+    println(exceptionsRequire(9))
+    try {
+        println(exceptionsRequire(-1)) //should throw require exception
     }
+    catch(e: Exception){
+        println(e.stackTraceToString())
+    }
+
+    try {
+        println(exceptionsRequire(0)) //should throw Illegal Arg exception
+    }
+    catch(e: Exception){
+        println(e)
+    }
+
+    try {
+        println(exceptionsRequire(10)) //should throw check exception
+    }
+    catch(e: Exception){
+        println(e)
+    }
+
+    try {
+        println(exceptionsRequire(7)) //should throw check exception
+    }
+    catch(e: Exception){
+        println(e)
+    }
+
+    lambda()
+
+    extension()
+
+    nullSafety()
 }
+
+fun exceptionsRequire(input: Int): Int {
+    require(input>-1){"Input needs to be positive"}
+
+    val cause = IllegalStateException("Original Cause: Illegal State")
+    if(0 == input) {
+        throw IllegalArgumentException("Input cannot be 0", cause)
+    }
+
+    check(input != 10) {"Input cannot be 10"}
+
+    if(7 == input)
+        error("Oops....Lucky number called!")
+
+    return input
+}
+
+fun lambda() {
+    val items = listOf(1, 2, 3, 4, 5, 6)
+
+    // Lambdas are code blocks enclosed in curly braces.
+    items.fold(100, {
+        // When a lambda has parameters, they go first, followed by '->'
+            acc: Int, i: Int ->
+        print("acc = $acc, i = $i, ")
+        val result = acc + i
+        println("result = $result")
+        // The last expression in a lambda is considered the return value:
+        result
+    })
+
+    // Parameter types in a lambda are optional if they can be inferred:
+    val joinedToString = items.fold("Elements:", { acc, i -> "$acc $i" })
+    println(joinedToString)
+
+    // Function references can also be used for higher-order function calls:
+    val product = items.fold(1, Int::times)
+    println(product)
+
+    val stringPlus: (String, String) -> String = String::plus
+
+    println(stringPlus("hello, ", "world!"))
+
+    val lambdaSum: (Int, Int) -> Int = {a: Int, b: Int -> a+b}
+
+    println(lambdaSum(1,7))
+
+    val lambdaSquare: (Int) -> Int = {a: Int -> a*a}
+
+    println(lambdaSquare(7))
+}
+
+fun extension(){
+    open class Shape
+    class Rectangle: Shape()
+
+    fun Shape.getName() = "Shape"
+    fun Rectangle.getName() = "Rectangle"
+
+    fun printClassName(s: Shape){
+        println(s.getName())
+    }
+
+    println(Rectangle().getName())
+    printClassName(Shape())
+    printClassName(Rectangle())
+}
+
+fun nullSafety() {
+    var line: String = "my line"
+    println(line)
+
+    //line = null
+
+}
+
+
+/**
+ * -- Lambda expressions + Inline functions = performant custom control structures
+ *
+ * -- Extension functions
+ *
+ * Null-safety
+ *
+ * Smart casts (Java 16: Pattern Matching for instanceof)
+ *
+ * String templates (Java 21: String Templates (Preview))
+ *
+ * Properties
+ *
+ * Primary constructors
+ *
+ * First-class delegation
+ *
+ * Type inference for variable and property types (Java 10: Local-Variable Type Inference)
+ *
+ * Singletons
+ *
+ * Declaration-site variance & Type projections
+ *
+ * Range expressions
+ *
+ * Operator overloading
+ *
+ * Companion objects
+ *
+ * Data classes
+ *
+ * Coroutines
+ *
+ * Top-level functions
+ *
+ * Default arguments
+ *
+ * Named parameters
+ *
+ * Infix functions
+ *
+ * Expect and actual declarations
+ *
+ * Explicit API mode and better control of API surface
+ *
+ * */
